7f181cb6ebbe61f4b2957bc8a8d593c033ab5884
Hunter Birch <hunter.macka.birch@gmail.com>
2022-06-08 16:36:21 -0600

    Core lab complete

---
+++ index.js
@@ -0,0 +1,42 @@
+document.querySelector("form").addEventListener("submit", addMovie);
+let message = document.querySelector('#message')
+
+function addMovie(event) {
+
+    event.preventDefault();
+
+    let inputField = document.querySelector('input');
+
+    let movie = document.createElement('li');
+    
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie)
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn)
+
+    document.querySelector("ul").appendChild(movie)
+
+    inputField.value = ''
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+    message.textContent = 'Movie Deleted!'
+}
+
+function crossOffMovie(event) {
+        event.target.classList.toggle('checked')
+
+        if (event.target.classList.contains('checked') === true) {
+        message.textContent = 'Movie Watched'
+        } else {
+            message.textContent = 'Movie Added Back!'
+        }
+}
+
+
